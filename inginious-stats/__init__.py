3e50ecf80d626e244d4ae5c0d9f237dc02a05334
Maxmaw <max.mawait@gmail.com>
2019-07-02 00:05:29 +0200

    setup base plugin

--- inginious-stats/__init__.py
+++ inginious-stats/__init__.py
@@ -2,6 +2,30 @@ import os
 import web
 
 from inginious.frontend.pages.course_admin.utils import INGIniousAdminPage
+from datetime import datetime, date, timedelta
+
+PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
+
+
+def add_admin_menu(course): # pylint: disable=unused-argument
+    """ Add a menu for the contest settings in the administration """
+    return ('adv_stats', '<i class="fa fa-trophy fa-fw"></i>&nbsp; Advanced statistics')
+
+
+class StaticMockPage(object):
+    # TODO: Replace by shared static middleware and let webserver serve the files
+    def GET(self, path):
+        if not os.path.abspath(PATH_TO_PLUGIN) in os.path.abspath(os.path.join(PATH_TO_PLUGIN, path)):
+            raise web.notfound()
+
+        try:
+            with open(os.path.join(PATH_TO_PLUGIN, "static", path), 'rb') as file:
+                return file.read()
+        except:
+            raise web.notfound()
+
+    def POST(self, path):
+        return self.GET(path)
 
 
 class AdvancedCourseStatisticClass(INGIniousAdminPage):
@@ -9,8 +33,32 @@ class AdvancedCourseStatisticClass(INGIniousAdminPage):
     def GET_AUTH(self, courseid, f=None, t=None):
         """ GET Request """
 
-        pass # return the template renderer
+        course, __ = self.get_course_and_check_rights(courseid)
+        tasks = course.get_tasks()
+        now = datetime.now().replace(minute=0, second=0, microsecond=0)
+
+        error = None
+        if f == None and t == None:
+            daterange = [now - timedelta(days=14), now]
+        else:
+            try:
+                daterange = [datetime.strptime(x[0:16], "%Y-%m-%dT%H:%M") for x in (f, t)]
+            except:
+                error = "Invalid dates"
+                daterange = [now - timedelta(days=14), now]
+
+        return self.template_helper.get_custom_renderer(os.path.join(PATH_TO_PLUGIN, 'templates')).adv_stats(course)
+
+    def POST_AUTH(self, courseid):
+        """ POST Request"""
+        pass
 
 
-def init(plugin_manager, course_factory, client, plugin_config):
-    plugin_manager.add_page('/plugins/stats/(.+)', AdvancedCourseStatisticClass)
+def init(plugin_manager, course_factory, client, plugin_config):  # pylint: disable=unused-argument
+    """ Init the plugin """
+    plugin_manager.add_page('/admin/([^/]+)/adv_stats', AdvancedCourseStatisticClass)
+    plugin_manager.add_hook('course_admin_menu', add_admin_menu)
+    plugin_manager.add_page('/plugins/stats/static/(.+)', StaticMockPage)
+    plugin_manager.add_hook('css', lambda: '/plugins/stats/static/adv_stats.css')
+    plugin_manager.add_hook('javascript_header', lambda: '/plugins/stats/static/adv_stats.js')
+

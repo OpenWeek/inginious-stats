2dab4f366cd59b2ea6d307ba049906b2eb9a0332
sigustin <simon.gustin@student.uclouvain.be>
2019-07-02 11:53:13 +0200

    Fetch submitted data from JS code

--- inginious-stats/static/adv_stats.js
+++ inginious-stats/static/adv_stats.js
@@ -1,4 +1,7 @@
 //js for template
 function submitForm() {
     alert("Submitting form");
+    let from = $("#stats_from").val();
+    let to = $("#stats_to").val();
+    alert("Data: from: " + from + " to: " + to);
 }

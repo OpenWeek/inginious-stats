65ea681781186c31e5b55bb4670cf08d0bc8cee0
sigustin <simon.gustin@student.uclouvain.be>
2019-07-02 12:10:56 +0200

    Fix JS url

--- inginious-stats/static/adv_stats.js
+++ inginious-stats/static/adv_stats.js
@@ -1,7 +1,9 @@
 //js for template
+
 function submitForm() {
     alert("Submitting form");
     let from = $("#stats_from").val();
     let to = $("#stats_to").val();
     alert("Data: from: " + from + " to: " + to);
 }
+
